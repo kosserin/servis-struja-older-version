f3d3efb0c42bffc5b4bd6f9087a477e29c350049
Kostic <kostic.andrija00@gmail.com>
2021-03-25 13:19:58 +0100

    d

--- carousel.js
+++ carousel.js
@@ -2,7 +2,7 @@ const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const auto = true; // Auto scroll
-const intervalTime = 3000;
+const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
